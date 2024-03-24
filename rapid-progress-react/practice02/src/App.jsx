fa22b3490d601f226a5e5456bce3d31c5f595057
subin114 <bin7851@naver.com>
2024-03-24 22:08:40 +0900

    비동기로 이루어지는 setState

---
+++ rapid-progress-react/practice02/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+// import Counter from "./components/Counter";
+import Counter2 from "./components/Counter2";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter2 />
+    </div>
+  );
+}
+
+export default App;
