93e045a98e7e7dcc74b09b91b4f055f0f7527cd9
subin114 <bin7851@naver.com>
2024-04-05 16:30:02 +0900

    코드 수정

--- js-study-100-solve/005.js
+++ js-study-100-solve/005.js
@@ -11,9 +11,9 @@
 */
 
 const solution = (angle) => {
-  if (0 < angle && angle < 90) return 1;
+  if (angle > 0 && angle < 90) return 1;
   else if (angle === 90) return 2;
-  else if (90 < angle && angle < 180) return 3;
+  else if (angle > 90 && angle < 180) return 3;
   else if (angle === 180) return 4;
   else return -1;
 };
