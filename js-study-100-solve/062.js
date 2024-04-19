2c069884cc109794dd6a2f9aaaeb74be6c683581
subin114 <bin7851@naver.com>
2024-04-19 14:37:06 +0900

    코드 수정

--- js-study-100-solve/062.js
+++ js-study-100-solve/062.js
@@ -9,7 +9,7 @@ const solution = nums =>
   nums
     .join('')
     .split('')
-    .filter(n => String(n).includes('7')).length
+    .filter(n => n.includes('7')).length
 
 console.log(solution([7, 77, 17])) // 4
 console.log(solution([10, 29])) // 0
