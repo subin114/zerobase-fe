8b8b3016fd13f639acf11eb57130547a6f5f474f
subin114 <bin7851@naver.com>
2024-05-03 22:57:19 +0900

    폼에서 다중 입력 다루기

--- rapid-progress-react/practice08/src/components/SimpleForm.jsx
+++ rapid-progress-react/practice08/src/components/SimpleForm.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
 
 const SimpleForm = () => {
-  const [nickname, setNickName] = useState("");
+  // const [nickname, setNickName] = useState("");
+  // const [password, setPassword] = useState("");
+
+  const [userInputs, setUserInputs] = useState({
+    nickname: "",
+    password: "",
+  });
 
   const handleChange = (e) => {
-    setNickName(e.target.value);
+    setUserInputs({ ...userInputs, [e.target.name]: e.target.value });
+    // if (e.target.name === "nickname") {
+    //   return setUserInputs(e.target.value);
+    // } else {
+    //   return setUserInputs(e.target.value);
+    // }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // 서버 요청 + a
-    alert(nickname);
+    const { nickname, password } = userInputs;
+    alert(`nickname : ${nickname}, password : ${password}`);
   };
 
   return (
@@ -20,8 +31,17 @@ const SimpleForm = () => {
         type="text"
         name="nickname"
         onChange={handleChange}
-        value={nickname}
+        value={userInputs.nickname}
       />
+      <br />
+      <label>패스워드 : </label>
+      <input
+        type="text"
+        name="password"
+        onChange={handleChange}
+        value={userInputs.password}
+      />
+      <br />
       <input type="submit" value="제출" />
     </form>
   );
