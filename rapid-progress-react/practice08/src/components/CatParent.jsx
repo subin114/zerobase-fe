3bac7f94168340eb9b9f01c760bf11f414da8da6
subin114 <bin7851@naver.com>
2024-05-07 17:33:30 +0900

    ref 심화 - mutable object

--- rapid-progress-react/practice08/src/components/CatParent.jsx
+++ rapid-progress-react/practice08/src/components/CatParent.jsx
@@ -1,5 +1,5 @@
 import Cat from "./Cat";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 // Cat 안에 있는 img 사이즈를 알고 싶다!
 export default function CatParent() {
@@ -7,14 +7,23 @@ export default function CatParent() {
   console.log("부모 컴포넌트 CatParent");
   console.log(catRef);
 
+  const ageRef = useRef(1);
+  const [state, setState] = useState(1);
+
   return (
     <div>
       <h4> 고양이가 세상을 구한다</h4>
+      <h3>나이: {ageRef.current}살</h3>
+      <h4>상태값: {state}</h4>
       <div>
         <Cat a={"a"} ref={catRef} />
         <button onClick={() => alert(catRef.current.height)}>
           고양이의 크기를 알고싶어
         </button>
+        <button onClick={() => (ageRef.current = ageRef.current + 1)}>
+          새해
+        </button>
+        <button onClick={() => setState(state + 1)}>테스트</button>
       </div>
     </div>
   );
