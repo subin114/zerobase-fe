d4e79ef0786e5d36bee6bdf3a553f129fd89dba5
subin114 <bin7851@naver.com>
2024-05-30 16:36:33 +0900

    fetch API 사용

--- rapid-progress-react/practice10/src/components/ReactPage.jsx
+++ rapid-progress-react/practice10/src/components/ReactPage.jsx
@@ -1,33 +1,50 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const ReactPage = () => {
-  const docs = [
-    {
-      id: 1,
-      title: "리액트 공부를 시작하면서",
-      date: "05/25/2024",
-    },
-    {
-      id: 2,
-      title: "Vite로 리액트 프로젝트 시작하기",
-      date: "05/26/2024",
-    },
-    {
-      id: 3,
-      title: "이제는 사용해보자 useMemo & useCallback",
-      date: "05/27/2024",
-    },
-    {
-      id: 4,
-      title: "리액트 프로젝트에 Prettier 적용하기",
-      date: "05/28/2024",
-    },
-    {
-      id: 5,
-      title: "리액트의 setState() 제대로 사용하기",
-      date: "05/29/2024",
-    },
-  ];
+  const [docs, setDocs] = useState([]);
+
+  // const docs = [
+  //   {
+  //     id: 1,
+  //     title: "리액트 공부를 시작하면서",
+  //     date: "05/25/2024",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Vite로 리액트 프로젝트 시작하기",
+  //     date: "05/26/2024",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "이제는 사용해보자 useMemo & useCallback",
+  //     date: "05/27/2024",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "리액트 프로젝트에 Prettier 적용하기",
+  //     date: "05/28/2024",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "리액트의 setState() 제대로 사용하기",
+  //     date: "05/29/2024",
+  //   },
+  // ];
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const result = await res.json(res);
+      // console.log(result);
+      // setDocs(result);
+      return result;
+    }
+
+    fetchData().then((res) => {
+      setDocs(res);
+    });
+  }, []);
 
   return (
     <>
