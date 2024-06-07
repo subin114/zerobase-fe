0756c9370497351f56706cb9b9e7e3dacffc0d10
subin114 <bin7851@naver.com>
2024-06-08 02:28:11 +0900

    Github Issue 실습 (API 호출)

--- starting-from-scratch/src/App.jsx
+++ starting-from-scratch/src/App.jsx
@@ -2,8 +2,18 @@ import styles from "./App.module.css";
 import Header from "./layout/Header/Header";
 import ListContainer from "./layout/ListContainer/ListContainer";
 import Footer from "./layout/Footer/Footer";
+import axios from "axios";
 
 function App() {
+  async function getData() {
+    const data = await axios.get(
+      `https://api.github.com/repos/facebook/react/issues`
+    );
+    console.log({ data });
+  }
+
+  getData();
+
   return (
     <>
       <div className={styles.nav}>Nav</div>
